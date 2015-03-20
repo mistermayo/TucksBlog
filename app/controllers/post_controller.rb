33e6adcb5b94530440a5b3dfad39f51b7f05cbff
tucker <tuckerjamesmayo@gmail.com>
2015-03-20 09:46:34 -0700

    add errors.erb page, add logic to post controller, add alert code to application erb

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params[:post])
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+end
