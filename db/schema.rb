036899416bc9162769cbbbb6dd25b204c72ff119
tucker <tuckerjamesmayo@gmail.com>
2015-03-27 12:02:19 -0700

    remove username feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150327172002) do
+ActiveRecord::Schema.define(version: 20150327185913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20150327172002) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "username"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 describe User do
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
-
 
   it { should have_many :posts }
 end
