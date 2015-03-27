20c052c11c3de1c8f75035ba161b22a4ce0bec9a
tucker <tuckerjamesmayo@gmail.com>
2015-03-27 09:56:40 -0700

    create user_spec file, add logic to user controller

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
 class Comment < ActiveRecord::Base
   validates :description, :presence => true
   belongs_to :post
-
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, :body, :date, :presence => true
   has_many :comments
-
+  belongs_to :user
 end
