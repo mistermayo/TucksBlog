0479a88110de79ea712642a99aea4297ac1280dc
tucker <tuckerjamesmayo@gmail.com>
2015-04-03 13:56:45 -0700

    refactor one million things to fix ajax bug with comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  respond_to :html, :js
 
   def new
     @post = Post.find(params[:post_id])
@@ -23,7 +22,7 @@ class CommentsController < ApplicationController
         flash[:notice] = "Error in comment submission."
         render :new
       end
-      format.js { render js: "alert('fail');" }
+      format.js
       end
     end
   end
@@ -51,25 +50,11 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
     if @comment.update(params[:comment])
-      if @comment.save
-        respond_to do |format|
-          format.html do
-            flash[:notice] = "Comment Posted"
-            redirect_to post_path(@post)
-          end
-          format.js
-        end
-      end
-      else
-        respond_to do |format|
-          format.html do
-            flash[:notice] = "Error in comment submission."
-            render :edit
-          end
-          format.js { render js: "alert('fail');" }
-        end
-      end
+      redirect_to post_path(@post)
+    else
+      render :edit
     end
+  end
 
   def comment_params
     params.require(:comment).permit(:description)
